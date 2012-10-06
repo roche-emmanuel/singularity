d59cf27a7ce0d734b8a39f4feb435fb22a09aefa
manu <roche.emmanuel@gmail.com>
2012-10-06 15:54:07 +0200

    Removed timer tests from lanes.

--- software/lua/modules/tests/suites/lanes.lua
+++ software/lua/modules/tests/suites/lanes.lua
@@ -6,13 +6,39 @@ function test_lane_timer()
 	log:info("Tests","Testing Lane timer");
 	
 	local lanes = require "lanes"
-	lanes.configure()
+	lanes.configure({with_timers=false})
+		
+	local f= lanes.gen( function(n) return 2*n end )
+	local a= f(1)
+	local b= f(2)
+
+	log:info("Tests","Lane results are, a=",a[1],", b=",b[1] )
+	  
+	--lanes = nil
+	--collectgarbage("collect")
 	
 	local linda= lanes.linda()
 	
+	--local ffi = require "ffi"
+	--ffi.cdef "unsigned int sleep(unsigned int seconds);"
+	--ffi.cdef "unsigned int usleep(unsigned int microseconds);"
+
+	count = 0
+	while true and count < 10 do
+	  local key,v= linda:receive( 0.1, "dummy_key")
+	  --ffi.C.sleep(1)
+	  --ffi.C.usleep(500000)
+	  
+	  --log:info("Tests","Timer "..key..": "..v )
+	  log:info("Tests","Timer : ".. lanes.now_secs() )
+	  count = count+1
+	end
+	
+	--[[
 	-- First timer once a second, not synchronized to wall clock
     --
 	lanes.timer( linda, "sec", 1, 1 )
+	
 	
 	-- Timer to a future event (next even minute); wall clock synchronized 
 	--
@@ -30,6 +56,7 @@ function test_lane_timer()
 
 	lanes.timer( linda, "sec", 0 )
 	lanes.timer( linda, "min", 0 )
+	]]
 	
 	log:info("Tests","Done testing Lane timer.")
 end
