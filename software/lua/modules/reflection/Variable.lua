3b8c919358c08e786856af49b54e6913a2565652
manu <roche.emmanuel@gmail.com>
2012-11-24 22:52:01 +0100

    Updated variable class.

--- software/lua/modules/reflection/Variable.lua
+++ software/lua/modules/reflection/Variable.lua
@@ -1,15 +1,3 @@
-local Class = require("classBuilder"){name="Class",bases="reflection.Member"}
-
---- Retrieve the full name of that Class.
--- The full name will take into account the parent scopes if any.
-function Class:getFullName()
-    if self:getParent() then
-        -- Assume the parent as a getFullName() function:
-        local pname = self:getParent():getFullName()
-        return (pname=="" and "" or (pname .. "::")) .. self:getName()
-    else
-        return self:getName()
-    end
-end
+local Class = require("classBuilder"){name="Variable",bases="reflection.Member"}
 
 return Class
