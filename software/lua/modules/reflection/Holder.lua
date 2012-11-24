f424c4e202cc6c78bc01a79069ecec0a79c666f8
manu <roche.emmanuel@gmail.com>
2012-11-24 18:25:44 +0100

    Removed IVariableHolder.

--- software/lua/modules/reflection/Holder.lua
+++ software/lua/modules/reflection/Holder.lua
@@ -1,11 +1,30 @@
 local Class = require("classBuilder"){name="Holder",bases=
 	{"reflection.Scope","reflection.IFunctionHolder",
-	 "reflection.IVariableHolder","reflection.IEnumHolder"}};
+	 "reflection.IEnumHolder"}};
 
 local Set = require "std.Set"
 
 function Class:initialize(options)
+	local ItemSet = require "reflection.ItemSet"
+	
+	self._variables = ItemSet();
+end
 
+function Class:getItems(list,filters,args)
+	return list:filterItems(filters,args)
+end
+
+function Class:getVariables(filters,args)
+   return self:getItems(self._variables,filters,args)
+end
+
+function Class:addVariable(var)
+	self:check(var,"Invalid var argument.")
+	self:checkType(var,require"reflection.Variable")
+
+	var:setParent(self)
+	
+	self._variables:addItem(var)
 end
 
 return Class
