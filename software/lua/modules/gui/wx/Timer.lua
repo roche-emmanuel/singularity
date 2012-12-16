e9a8cc7b278337579a898a3c4b16c58f4e9b181a
manu <roche.emmanuel@gmail.com>
2012-12-16 01:36:16 +0100

    Cleaned Timer class.

--- software/lua/modules/gui/wx/Timer.lua
+++ software/lua/modules/gui/wx/Timer.lua
@@ -7,32 +7,13 @@ function Class:initialize(options)
 	self:check(options and (options.frequency or options.delay), "Invalid inputs for Timer.")
 	self:check(options.callback, "Invalid callback.")
 	
-	--self:debug4("Creating extended timer.")
-	--self.wrapper = wx.wxTimer(self)
-	--self:debug4("Extended timer created.")
-	
-	-- Retrieve all the functions from the wrapped class:
-	--[[for name,func in pairs(wx.wxTimer) do
-		if(type(func)=="function" and not self[name]) and name~="new" and name~="__eq" and name~="__gc" then
-			--self:info("Adding auto wrapped function: ",name)
-			local wname = (name:sub(1,5)=="base_" and name) or (wx.wxTimer["base_"..name] and "base_"..name) or name -- force rediction to the base function call to avoid infinite looping.
-			self[name] = function(arg1, ...) self.wrapper[wname](self.wrapper, ...) end
-		end
-	end
-	]]
-	
 	self:generateWrapping(wx.wxTimer)
-	
 	
 	self._interval = math.floor((options.delay or 1.0/options.frequency)*1000.0 + 0.5);
 	self._callback = options.callback
 	self._arg = options.arg
 	self._oneShot = options.oneShot or false
 end
-
---function Class:release()
---	self.wrapper = nil;
---end
 
 function Class:Notify()
 	--self:info("Calling notify on extended timer.")
