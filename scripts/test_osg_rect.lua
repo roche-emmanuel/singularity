4880e42898c3e73ac7e3608fd5de27e057695534
manu <roche.emmanuel@gmail.com>
2012-12-27 14:59:17 +0100

    Added some web page tests.

--- scripts/test_osg_rect.lua
+++ scripts/test_osg_rect.lua
@@ -35,7 +35,10 @@ local onStartFunc = function(parent)
 	canvas:getRoot():addChild(obj:getWrapper())
 	-- obj:loadURL("http://www.google.fr")
 	-- obj:loadURL("http://www.smashcat.org/av/canvas_test/")
-	obj:loadURL("http://www.doesmybrowsersupportwebgl.com/")
+	-- obj:loadURL("http://oos.moxiecode.com/js_webgl/woods_xmas/")
+	-- obj:loadURL("http://www.asterank.com/3d/")
+	obj:loadURL("http://asmallgame.com/labsopen/webgl_impact/")
+	-- obj:loadURL("http://www.doesmybrowsersupportwebgl.com/")
 end
 
 local app = App{onStart=onStartFunc,profileFile="test_osg_profile.log"}
