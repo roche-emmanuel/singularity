63583199b7818b1bfe0ad179fcb8c1b681c66570
manu <roche.emmanuel@gmail.com>
2013-01-31 21:20:40 +0100

    Adding tests 6 and 7 for awesomium module.

--- scripts/test_web.lua
+++ scripts/test_web.lua
@@ -6,8 +6,10 @@ log:notice "Executing init script..."
 local App = require "gui.web.BookApp"
 local app = App{profileFile="test_web_profile.log",
 	pages={
-		fs:getRootPath(true).."tests/data/web/test4.html",
-		fs:getRootPath(true).."tests/data/web/test5.html",
+		-- fs:getRootPath(true).."tests/data/web/test4.html",
+		-- fs:getRootPath(true).."tests/data/web/test5.html",
+		-- fs:getRootPath(true).."tests/data/web/test6.html",
+		fs:getRootPath(true).."tests/data/web/test7.html",
 	}
 }
 
