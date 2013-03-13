3051932f88adae70efb835fe8378a561cd2f468d
manu <roche.emmanuel@gmail.com>
2013-03-13 09:03:59 +0100

    Refactoring Genesis overlays.

--- projects/genesis/web/js/overlays.js
+++ projects/genesis/web/js/overlays.js
@@ -1,4 +1,35 @@
-window.onload = function(){
+
+$(function(){
+	// update the block positions:
+	// positions are given in number of blocks from the top/left corner
+	
+	var sw = 80; // screen width
+	var sh = 36; // screen height
+	var xr = 100.0/sw;
+	var yr = 100.0/sh;
+	
+	var setLeft = function(select,pos) {
+		$(select).css('left', pos*xr + "%");
+	}
+
+	var setTop = function(select,pos) {
+		$(select).css('top', pos*yr + "%");
+	}
+	
+	setTop('.top',1)
+	
+	setLeft('.left',1)
+	setLeft('.sensor_l1',12)
+	setLeft('.sensor_l2',18)
+	setLeft('.sensor_l3',25)
+	setLeft('.sensor_c',36)
+	setLeft('.sensor_r1',48)
+	setLeft('.sensor_r2',55)
+	setLeft('.sensor_r3',61)
+	setLeft('.sensor_r4',67)
+	setLeft('.sensor_r5',74)
+	
+	
 	var test = document.getElementById("Compute");
 	test.style.fontSize = 20.0;
 	var height = (test.clientHeight);
@@ -15,4 +46,5 @@ window.onload = function(){
 	
 	var test = document.getElementById("output");
 	output.innerHTML = "A size is: "+width+"x"+height+". Setting letter spacing to "+ls+"px";
-}
+})
+
