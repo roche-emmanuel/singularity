b1db64b31a7499fd797e8328f4c02e171733e570
manu <roche.emmanuel@gmail.com>
2013-04-09 23:28:40 +0200

    Removed obsolete lua script files (moved in shared script location).

--- scripts/get_version_number.lua
+++
@@ -1,8 +0,0 @@
-#!/bin/sgt
-version_path=arg[1]:gsub("\\","/")
-
--- print("Version path is: ", version_path)
-package.path= version_path.."/?.lua;" .. package.path
-
-local version = require "version"
-io.write(version[arg[2]])
