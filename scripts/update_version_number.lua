1f6ecff26a6563046e20ae5afcff984f04f45682
manu <roche.emmanuel@gmail.com>
2013-04-09 18:34:21 +0200

    Updated version number update script to allow setting version from argument.

--- scripts/update_version_number.lua
+++ scripts/update_version_number.lua
@@ -2,6 +2,8 @@
 
 version_path=arg[1]:gsub("\\","/")
 
+nver=arg[2]
+
 -- print("Version path is: ", version_path)
 package.path= version_path.."/?.lua;" .. package.path
 
@@ -20,6 +22,14 @@ version.date = "%s"
 
 return version;
 ]]
+
+if nver then
+	print("Parsing version number: ",nver)
+	local p1,p2,major,minor,patch = nver:find("(%d+)%.(%d+)%.(%d+)")
+	version.major = major+0
+	version.minor = minor+0
+	version.patch = patch+0
+end
 
 local file = io.open(version_path.."/version.lua","w")
 file:write(content:format(version.major,version.minor,version.patch,version.build + 1,os.date()))
