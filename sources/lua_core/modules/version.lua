af1fe00baf514c9a12f659de5e519e177250b8de
manu <roche.emmanuel@gmail.com>
2013-05-02 10:54:19 +0200

    Now considering long as number instead of integer to avoid out of range integer values.

--- sources/lua_core/modules/version.lua
+++ sources/lua_core/modules/version.lua
@@ -4,7 +4,7 @@ version.major = 0
 version.minor = 3
 version.patch = 0
 
-version.build = 981
-version.date = "04/27/13 09:40:09"
+version.build = 982
+version.date = "05/02/13 09:53:05"
 
 return version;
