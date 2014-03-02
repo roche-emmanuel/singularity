7c0cd3afbd40c4016a6c0b23d5207c8f37a2c966
manu <roche.emmanuel@gmail.com>
2014-03-02 20:05:31 +0100

    Added complete support for MTOverlay.

--- projects/mxengine/modules/aw/WebViewDX.lua
+++ projects/mxengine/modules/aw/WebViewDX.lua
@@ -5,6 +5,16 @@ local awe = require "Awesomium"
 function Class:initialize()
 
 	self._cache = {}
+	self._texts = {}
+	self._highlights = {}
+	self._flashings = {}
+	self._widgets = {}
+	self._groups = {}
+	self._reticlePos = {}
+	self._reticleSize = {}
+	self._avt_alt_targets = {}
+	self._streamTexts = {}
+	self._streamHighlights = {}
 
 	self:onOverlayReady(function()
 		self:debug("Overlay is now ready.")
@@ -56,19 +66,94 @@ function Class:setTargetSurface(surface)
 	end
 end
 
-function Class:performFullRefresh()
-	self:setMenuMap()
-	self:setLayout()
-	self:setGimbalAzimuthAngle()
-	self:setGimbalElevationAngle()
-end
-
-function Class:getCache(key,val)
-	if val~=nil then
+function Class:getCache(key,val,defval)
+	if val == nil then
+		val = self._cache[key]
+	else
 		self._cache[key] = val
 	end
 
-	return self._cache[key] 
+	if val == nil then
+		return defval
+	end
+
+	return val 
+end
+
+function Class:performFullRefresh()
+	self:setMenuMap()
+	self:setLayout()
+	self:setNorthIndicatorAngle()
+	self:setMXPODAzimuthAngle()
+	self:setMXPODFOVAngle()
+	self:setGimbalAzimuthAngle()
+	self:setGimbalElevationAngle()
+	self:setPictoFOVAngle()
+
+	if self._overlayColor then
+		local col = self._overlayColor
+		self:call("setOverlayColor",col[1],col[2],col[3],col[4])
+	end
+
+	if self._currentStream then
+		local sname = self._currentStream
+		local list = self._streamTexts[sname] or {}
+		for name, value in pairs(list) do
+			self:call("setTextValue",name,value)
+		end
+		local list = self._streamHighlights[sname] or {}
+		for name, value in pairs(list) do
+			self:call("setTextHighlight",name,value)
+		end		
+	end
+
+	if self._avt_target then
+		local desc = self._avt_target
+		self:call("setAVTTargetState",desc[1],desc[2],desc[3],desc[4],desc[5])
+	end
+
+	if self._sens_gate then
+		local desc = self._sens_gate
+		self:call("setSensitivityGateState",desc[1],desc[2],desc[3])
+	end
+
+	for index,desc in pairs(self._avt_alt_targets) do
+		self:call("setAVTAltTargetState",desc[1],desc[2],desc[3],desc[4],desc[5])
+	end
+
+	if self._acqWinState then
+		self:call("setAcquisitionWindowState",self._acqWinState[1],self._acqWinState[2],self._acqWinState[3])
+	end
+
+	for iname, value in pairs(self._texts) do
+		self:call("setTextValue",iname,value)
+	end
+
+	for iname, value in pairs(self._reticlePos) do
+		self:call("setReticlePosition",iname,value[1],value[2])
+	end
+
+	for iname, value in pairs(self._reticleSize) do
+		self:call("setReticleSize",iname,value[1],value[2])
+	end
+
+	for iname, value in pairs(self._widgets) do
+		self:call("enableWidget",iname,value)
+	end
+
+	for gname, value in pairs(self._groups) do
+		self:call("showGroup",gname,value)
+	end
+
+	for iname, value in pairs(self._highlights) do
+		self:call("setTextHighlight",iname,value,self._flashings[iname])
+	end
+
+	for iname, value in pairs(self._flashings) do
+		if not self._highlights[iname] then
+			self:call("setTextFlashing",iname,value)
+		end
+	end
 end
 
 function Class:setMenuMap(mmap)
@@ -80,27 +165,171 @@ function Class:setMenuMap(mmap)
 end
 
 function Class:setLayout(otype,res,flavor)
-	otype = self:getCache("layout_type",otype)
-	res = self:getCache("layout_res",res)
-	flavor = self:getCache("layout_flavor",flavor or "")
+	otype = self:getCache("layout_type",otype,"Type6")
+	res = self:getCache("layout_res",res,"HD")
+	flavor = self:getCache("layout_flavor",flavor,"")
 
 	if not otype then return end
 	self:debug("Setting layout to type=",otype,", res=",res,", flavor=",flavor)
 	self:call("setLayout",otype,res,flavor or "")	
 end
 
-function Class:setGimbalAzimuthAngle(angle)
-	angle = self:getCache("gimbal_azimuth",angle)
+function Class:setReticlePosition(rid,xx,yy)
+	self._reticlePos[rid] = { xx, yy }
+	self:call("setReticlePosition",rid,xx,yy)
+end
 
-	if not angle then return end
+function Class:setReticleSize(rid,ww, hh)
+	self._reticleSize[rid] = { ww, hh }
+	self:call("setReticleSize",rid,ww,hh)
+end
+
+function Class:setMenuVisibility(visible)
+	visible = self:getCache("menu_visible",visible,false)
+	if visible then
+		self:call("showPage","menu","done")
+	else
+		self:call("showPage","main_overlays")
+	end
+end
+
+function Class:setCurrentStream(sname)
+	if self._currentStream ~= sname then
+		self._currentStream = sname
+		local list = self._streamTexts[sname] or {}
+		for name, value in pairs(list) do
+			self:call("setTextValue",name,value)
+		end
+		local list = self._streamHighlights[sname] or {}
+		for name, value in pairs(list) do
+			self:call("setTextHighlight",name,value)
+		end
+	end
+end
+
+function Class:setStreamField(sname, item_name, value)
+	self._streamTexts[sname] = self._streamTexts[sname] or {}
+	local list = self._streamTexts[sname]
+	list[item_name] = value
+	-- self:info("received stream field value : ",item_name," = ", value, " on stream ", sname)
+
+	if sname == self._currentStream then
+		-- self:info("setting stream field value : ",item_name," = ", value, " on stream ", sname)
+		self:call("setTextValue",item_name, value)
+	end
+end
+
+function Class:setStreamHighlight(sname, item_name, value)
+	self._streamHighlights[sname] = self._streamHighlights[sname] or {}
+	local list = self._streamHighlights[sname]
+	list[item_name] = value
+
+	if sname == self._currentStream then
+		self:call("setTextHighlight",item_name, value)
+	end
+end
+
+function Class:setOverlayVisibility(visible)
+	visible = self:getCache("overlay_visible",visible,false)
+	if visible then
+		self:call("showPage","main_overlays")
+	end
+end
+
+function Class:setMenuSelection(menu_name,item_name,sub_index)
+	menu_name = self:getCache("selection_menu_name",menu_name)
+	item_name = self:getCache("selection_item_name",item_name)
+	sub_index = self:getCache("selection_sub_index",sub_index or false)
+
+	if sub_index == false then
+		sub_index = nil
+	end
+
+	if menu_name and item_name and self:getCache("menu_visible",nil,false) then
+		self:call("showPage",menu_name,item_name,sub_index)
+	end
+end
+
+function Class:showGroup(gname,value)
+	self._groups[gname] = value
+	self:call("showGroup",gname,value)
+end
+
+function Class:enableWidget(widget_name,value)
+	self._widgets[widget_name] = value
+	self:call("enableWidget",widget_name,value)
+end
+
+function Class:setTextValue(item_name,value)
+	self._texts[item_name] = value
+	self:call("setTextValue",item_name,value)
+end
+
+function Class:setTextFlashing(item_name,value)
+	self._flashings[item_name] = value
+	self:call("setTextFlashing",item_name,value)
+end
+
+function Class:setTextHighlight(item_name,value,flashing)
+	self._highlights[item_name] = value
+	self:call("setTextHighlight",item_name, value, flashing)
+end 
+
+function Class:setNorthIndicatorAngle(angle)
+	angle = self:getCache("north_indicator_angle",angle,0.0)
+	self:call("setNorthIndicatorAngle",angle)
+end
+
+function Class:setMXPODAzimuthAngle(angle)
+
+	angle = self:getCache("antenna_bearing_angle",angle,0.0)
+	self:call("setMXPODAzimuthAngle",value)
+end
+
+function Class:setMXPODFOVAngle(angle)
+	angle = self:getCache("antenna_fov_angle",angle,0.0)
+	self:call("setMXPODFOVAngle",angle)
+end
+
+function Class:setGimbalAzimuthAngle(angle)
+	angle = self:getCache("gimbal_azimuth",angle,0.0)
 	self:call("setGimbalAzimuthAngle",angle)
 end
 
 function Class:setGimbalElevationAngle(angle)
-	angle = self:getCache("gimbal_elevation",angle)
-
-	if not angle then return end
+	angle = self:getCache("gimbal_elevation",angle,0.0)
 	self:call("setGimbalElevationAngle",angle)
 end
+
+function Class:setPictoFOVAngle(angle)
+	angle = self:getCache("picto_fov_angle",angle,0.0)
+	self:call("setPictoFOVAngle",angle)
+end
+
+function Class:setOverlayColor(r,g,b,a)
+	self._overlayColor = { r,g,b,a }
+	self:call("setOverlayColor",r,g,b,a)
+end
+
+function Class:setAcquisitionWindowState(visible, ww, hh)
+	self._acqWinState = { visible, ww, hh }
+	self:call("setAcquisitionWindowState",visible,ww,hh)
+end
+
+function Class:setAVTTargetState(visible,xx,yy,ww,hh)
+	self._avt_target = { visible, xx, yy, ww, hh}
+	self:call("setAVTTargetState",visible,xx,yy,ww,hh)
+end
+
+function Class:setAVTAltTargetState(index,visible,xx,yy,ww,hh)
+	self._avt_alt_targets[index] = { visible, xx, yy, ww, hh}
+	self:call("setAVTAltTargetState",index,visible,xx,yy,ww,hh)
+end
+
+function Class:setSensitivityGateState(visible, ww,hh)
+	self._sens_gate = { visible, ww, hh}
+	self:call("setSensitivityGateState",visible,ww,hh)
+end
+
 
 return Class
--- projects/mxengine/modules/display/effects/MTOverlay.lua
+++ projects/mxengine/modules/display/effects/MTOverlay.lua
@@ -30,12 +30,32 @@ function Class:initialize(options)
 		-- self:showMessage("Done destroying web view: "..self._viewId)
 	end}
 
+	self:getTurret():addListener{Class.EVT_POST_UPDATE,function()		
+		self:updateSourceStream()
+		--self:updateOverlayContent()
+	end}	
+
 	-- On turret init we assign the menu map:
 	self:onTurretInit(function()
 		-- Now fill the array:
 		self:assignMenuMap()
 		self:assignLayout()
 	end)
+end
+
+function Class:getOutputChannel()
+	return self._processor
+end
+
+function Class:updateSourceStream()
+	local src = self:getOutputChannel():getSource()
+	
+	local sname = src:getName()	
+	
+	if self._currentStreamName~=sname then
+		self._currentStreamName = sname
+		self:onCurrentStreamUpdated(sname)
+	end 
 end
 
 function Class:assignMenuMap()
@@ -76,6 +96,88 @@ function Class:reload()
 	self:sendCommand("reload")
 end
 
+function Class:onMenuVisibilityUpdated(visible)
+	self:sendCommand("setMenuVisibility",visible)
+end
+
+function Class:onMenuSelectionUpdated(menu_name,item_name,sub_index)
+	self:sendCommand("setMenuSelection",menu_name,item_name,sub_index)
+end
+
+function Class:onMenuFieldUpdated(menu_name,item_name,value)
+	self:sendCommand("setTextValue",menu_name ..".".. item_name,value)
+end
+
+function Class:onCurrentStreamUpdated(sname)
+	-- self:showMessage("Updating current stream to ", sname)
+	self:sendCommand("setCurrentStream",sname)
+end
+
+local unit_map = {
+	[Enums.ANGLE_DEGREES] = "DD",
+	[Enums.ANGLE_DMS] = "DMS",
+	[Enums.ANGLE_DMF] = "DMF",
+	[Enums.ANGLE_MGRS] = "MGRS",
+}
+
+function Class:onPOILocationUnitUpdated(unit)
+	local val = unit_map[unit]
+	self:check(val,"Invalid mapping for POI location unit: ",unit)
+	-- TO RESTORE self._webView:call("setPOILocationMode",val)
+end
+
+function Class:onOverlayVisibilityUpdated(visible)
+	self:sendCommand("setOverlayVisibility",visible)
+end
+
+function Class:onFieldUpdated(item_name, value)
+	self:sendCommand("setTextValue",item_name, value)
+end
+
+function Class:onHighlightUpdated(item_name, value, flashValue)
+	-- flashValue = flashValue or false;
+	self:check(flashValue~=nil,"Invalid flash value !")
+	
+	if type(flashValue)=="table" and type(value)=="table" then
+		self:check(value[1]==flashValue[1] and value[2]==flashValue[2],"mismatch in highlight and flashing details.")
+	end
+	
+	self:sendCommand("setTextHighlight", item_name, value, flashValue and true or false)
+end
+
+function Class:onFlashingUpdated(item_name, value)
+	self:sendCommand("setTextFlashing", item_name, value)
+end
+
+function Class:onStreamFieldUpdated(sname, item_name, value)
+	self:sendCommand("setStreamField",sname,item_name, value)
+end
+
+function Class:onStreamHighlightUpdated(sname, item_name, value)
+	self:sendCommand("setStreamHighlight",sname,item_name, value)
+end
+
+function Class:onNorthIndicatorUpdated(value)
+	if(self._prevNorth and math.abs(self._prevNorth - value) < 0.01) then return end
+	self._prevNorth = value
+	self:sendCommand("setNorthIndicatorAngle",value)
+end
+
+function Class:onMXPODBearingUpdated(value)
+	self:sendCommand("setMXPODAzimuthAngle",value)
+end
+
+local antenna_fov = {
+	[Class.MX_POD_ANTENNA_AUTO] = 0.0,
+	[Class.MX_POD_ANTENNA_HIGH] = 60.0,
+	[Class.MX_POD_ANTENNA_OMNI] = 0.0,
+	[Class.MX_POD_ANTENNA_COUPLED] = 0.0,
+}
+
+function Class:onMXPODAntennaModeUpdated(value)
+	self:sendCommand("setMXPODFOVAngle",antenna_fov[value])
+end
+
 function Class:onGimbalAzimuthUpdated(value)	
 	if(self._prevAz and math.abs(self._prevAz - value) < 0.01) then return end
 	self._prevAz = value
@@ -86,6 +188,82 @@ function Class:onGimbalElevationUpdated(value)
 	if(self._prevElev and math.abs(self._prevElev - value) < 0.01) then return end
 	self._prevElev = value
 	self:sendCommand("setGimbalElevationAngle",value)
+end
+
+function Class:onPictoFOVUpdated(value)
+	self:sendCommand("setPictoFOVAngle",value)
+end
+
+
+function Class:onDestabilizationStateUpdated(value)
+	self:sendCommand("enableWidget","destabilization",value)
+end
+
+function Class:onTXEncryptionStateUpdated(value)
+	self:sendCommand("enableWidget","transmitter_key",value)
+end
+
+function Class:doSetOverlayGroupStatus(gid,status)
+	gid = gid:gsub("_group$","")
+	self:info("Setting group ",gid," to ",status)
+	self:sendCommand("showGroup",gid,status)
+end
+
+function Class:doSetReticleStatus(rid,enabled)
+	self:sendCommand("enableWidget",rid,enabled)
+end
+
+function Class:doSetReticlePosition(rid,pos)
+	self:sendCommand("setReticlePosition",rid,pos:x(),pos:y())
+end
+
+function Class:doSetReticleSize(rid,size)
+	self:sendCommand("setReticleSize",rid,size:x(),size:y())
+end
+
+function Class:doSetAcquisitionWindowState(visible, size)
+	self:sendCommand("setAcquisitionWindowState",visible,size:x(),size:y())
+end
+
+function Class:doSetAVTTargetState(visible,rect)
+	self:sendCommand("setAVTTargetState",visible,rect:x(),rect:y(),rect:z(),rect:w())
+end
+
+function Class:doSetAVTAltTargetState(visible,index,rect)
+	self:sendCommand("setAVTAltTargetState",index,visible,rect:x(),rect:y(),rect:z(),rect:w())
+end
+
+function Class:doSetGateState(visible, size)
+	self:sendCommand("setSensitivityGateState",visible,size:x(),size:y())
+end
+
+function Class:doSetOutlineState(enabled)
+	-- We do nothing here.
+end
+
+function Class:doSetOverlayColor(cid)
+	local col = self.overlayColorMap[cid]
+	self:check(col,"Invalid color for index ", cid)
+	local int = (self:getOverlayIntensity() or 100.0)/100.0
+
+	self:sendCommand("setOverlayColor",col:x()*int, col:y()*int, col:z()*int, 1.0)
+end
+
+function Class:doSetOverlayIntensity(intensity)
+	if self:useOverlayColor() then
+		local cid = self:getOverlayColor() or Enums.OVERLAY_COLOR_WHITE
+		local col = self.overlayColorMap[cid]
+		self:check(col,"Invalid color for index ", cid)
+		local int = intensity
+
+		self:sendCommand("setOverlayColor",col:x()*int, col:y()*int, col:z()*int, 1.0)
+	else
+		self:warn("old overlay intensity control not implemented")
+	end
+end
+
+function Class:doSetOverlayReverseIntensity(intensity)
+	self:warn("old overlay intensity control not implemented")
 end
 
 return Class
